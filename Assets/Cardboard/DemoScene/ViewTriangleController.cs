303615f94fe93fbb2eb51b64cdcee61827967ade
ace <andrew.emil@gmail.com>
2016-06-11 12:15:24 -0400

    Got viewtriangle working

---
+++ Assets/Cardboard/DemoScene/ViewTriangleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewTriangleController : MonoBehaviour {
+
+	public GameObject player;
+
+	private float currentRotation;
+
+	// Use this for initialization
+	void Start () {
+		currentRotation = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newRotation = getPlayerRotation ();
+		transform.Rotate (new Vector3(0, 0, currentRotation - newRotation));
+		currentRotation = newRotation;
+	}
+
+	private float getPlayerRotation() {
+		return player.transform.rotation.eulerAngles.y;
+	}
+}
